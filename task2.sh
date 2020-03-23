6be68b6947f25152a868e834cd5c3b9cde6a7167
Lilit-G <galstyan.lilit.00@gmail.com>
2020-03-23 23:39:10 +0400

    homework2

---
+++ task2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+grep -w -i "version" /etc/os-release
+echo "username:" $USER
+grep "model" /proc/cpuinfo | sort | uniq
+grep "cores" /proc/cpuinfo | sort | uniq
+hostnamectl | grep -i "kernel"
+
