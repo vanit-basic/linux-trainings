5c83dc2d31a945021f777f8c0b0109dd159ba2d8
095066956art <vanit-basic>
2020-03-25 17:42:53 +0400

    add

--- tatev2020martirosyan/task_2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "User:" `whoami`
-echo "OS type:" `cat /proc/sys/kernel/ostype`
-echo "Kernel version:" `cat /proc/sys/kernel/osrelease`
-echo "OS version:" `cat /proc/sys/kernel/version | awk '{print $1}'`
-echo "CPU cores:" `grep "cpu cores" /proc/cpuinfo | awk '{ print $4}'`
-echo "CPU model:" ` grep "model" /proc/cpuinfo | sed -n '1p' | awk '{print $3}'`
-echo "CPU model name:" `grep "model" /proc/cpuinfo | sed -n '2p' | awk '{ $1=""; $2="";$3="";print $0}'`
