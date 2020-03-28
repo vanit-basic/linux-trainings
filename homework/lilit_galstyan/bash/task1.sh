b34b673526606353160eb317b5629a7f6af06f08
Lilit-G <galstyan.lilit.00@gmail.com>
2020-03-29 01:02:51 +0400

    added second homework

--- homework/lilit_galstyan/bash/task1.sh
+++ homework/lilit_galstyan/bash/task1.sh
@@ -6,21 +6,19 @@ paths=`echo $PATH | sed 's/:/\n/g'`
 path=""
 
 
-        for i in $paths; do
-                p=$i/$name
-                test -f $p
-                if [ $? = 0 ]
-                then
-                path="$p"
-                        break;
-                fi;
-        done
+	for i in $paths; do 
+		p=$i/$name
+		test -f $p
+		if [ $? = 0 ]
+	       	then
+	  	path="$p"
+			break;
+        	fi;
+	done
 
-        if [ "$path" != "" ]
-        then
-                echo $path
-        else
-                echo "command is not found"
-        fi;
-
-
+	if [ "$path" != "" ]
+       	then
+		echo $path
+	else
+	       	echo "command is not found"
+	fi;
