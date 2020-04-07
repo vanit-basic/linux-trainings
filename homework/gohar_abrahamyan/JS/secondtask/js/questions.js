dc2fd5cfa355fa6366b6f8732359f69f05629f33
U-DESKTOP-KKKLHJT\USER <g0harabrahamyan@yandex.com>
2020-04-08 01:11:45 +0400

    added secondtask folder with js homework

---
+++ homework/gohar_abrahamyan/JS/secondtask/js/questions.js
@@ -0,0 +1,20 @@
+const data = [
+	{
+		id: '1',
+		question: "1. Which is the most populous continent?",
+		answers: ['america', 'asia', 'europ', 'africa'],
+		trueAnswer: 'asia'
+	},
+	{
+		id: '2',
+		question: "2. Which sport is played at Wimbledon?",
+		answers: ['tennis', 'voleyball', 'box', 'handball'],
+		trueAnswer: 'tennis'
+	},
+	{
+		id: '3',
+		question: "3. What is the capital of The United Kingdom?",
+		answers: ['newyork', 'bugenkhem', 'london', 'paris'],
+		trueAnswer: 'london'
+	},
+];
