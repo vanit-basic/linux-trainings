0160cc8b95c87e147470189319b76ef0a2312823
nare99 <nare.mkrtchyan99@mail.ru>
2020-04-08 17:34:05 +0400

    Added new homework

---
+++ homework/nare_mkrtchyan/JavaScript/Js/questions.js
@@ -0,0 +1,32 @@
+let data=[
+        {
+		id: '1',
+       		question: 'Which is the longest river?',
+        	answers: ['Nile','Amazon','Mississippi','Congo'],
+		trueAnswer: 'Amazon',
+        },
+        {
+		id: '2',
+	        question: 'Which is the biggest country?',
+		answers: ['China', 'USA', 'Russia', 'Canada'],
+	        trueAnswer: 'Russia',
+        },
+        {
+		id: '3',
+	        question: 'Which is the highest mountain?',
+	        answers: ['Everest', 'Ararat', 'Kilimanjaro', 'Vesuvius'],
+		trueAnswer: 'Everest',
+        },
+        {
+		id: '4',
+	        question: 'Who is the current world number three tennis player?',
+	        answers: ['Roger Federer', 'Rafael Nadal', 'Novak Djokovic', 'Dominic Thiem'],
+		trueAnswer: 'Dominic Thiem',
+        },
+        {
+		id: '5',
+	        question: 'Who is the captain of England national football team?',
+        	answers: ['Harry Kane', 'Jordan Pickford', 'Dele Alli', 'Raheem Sterling'],
+		trueAnswer: 'Harry Kane',
+        }
+]
