ad2cf0e382dc56c98552e230d40c00c46dff6992
Narek Iskandaryan <Nardaryan@mail.ru>
2020-04-10 17:15:38 +0400

    some changes homework js

--- homework/Narek_Iskandaryan/Javascript/task_3/js/questions.js
+++ homework/Narek_Iskandaryan/Javascript/task_3/js/questions.js
@@ -1,20 +1,26 @@
 const data = [
 	{
 		id: '1',
-		question: '2 + 2',
-		answers: ['1', '700', '4', '5'],
-		trueAnswer: '4'
+		question: 'What will 1 == "1" return?',
+		answers: ['True', '0', 'False', '1'],
+		trueAnswer: 'True'
 	},
 	{
 		id: '2',
-		question: 'What is the name of the earth satellite ?',
-		answers: ['venera', 'saturn', 'moon', 'armenia'],
-		trueAnswer: 'moon'
+		question: 'for(let x = 1; x < 5; x++)console.log(x) ?',
+		answers: ['11111', '12345', '1234', '5555'],
+		trueAnswer: '1234'
 	},
 	{
 		id: '3',
-		question: '2 + 10',
-		answers: ['11', '770', '120', '12'],
-		trueAnswer: '12'
+		question: 'Variable in JavaScript declared with which of the following keyword?',
+		answers: ['new', 'int', 'string', 'let'],
+		trueAnswer: 'let'
+	},
+    {
+		id: '4',
+		question: 'let x = 0; do{ console.log(x) }while(x > 0) ?',
+		answers: ['0', 'null', '1', 'No output'],
+		trueAnswer: '0'
 	},
 ];
