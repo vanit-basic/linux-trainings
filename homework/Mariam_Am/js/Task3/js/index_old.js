b1a0d385214078c88d48c782a007f4232be11b32
Mariam433 <amirxanyan.mariam.96844519@gmail.com>
2020-04-12 19:09:56 +0200

    Add JS homework

---
+++ homework/Mariam_Am/js/Task3/js/index_old.js
@@ -0,0 +1,40 @@
+
+const users = [];
+
+function validation() {
+    const name = document.getElementById('nameInput').value;
+    const email = document.getElementById('emailInput').value;
+    const password = document.getElementById('passwordInput').value;
+
+    if (name && email && password) {
+        addUser(name, email, password);
+    } else {
+        alert('Please enter all fields');
+        return;
+    }
+
+}
+
+function addUser(n, e, p) {
+    const user = {
+        name: n,
+        email: e,
+        password: p
+    };
+    users.push(user);
+    showMessageWindow(n);
+}
+
+function submitHandler(event) {
+    event.preventDefault();
+    //console.log(event.target.value);
+    validation();
+}
+
+function showMessageWindow(n) {
+    const modal = document.getElementById('modalWindow');
+    const body = modal.firstElementChild;
+    body.innerHTML = `user ${n} is added`;
+    modal.style.display = 'block';
+    setTimeout(function (){location.replace('index.html')},5000);
+}
