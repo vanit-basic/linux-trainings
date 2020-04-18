261058f242460debc7ff2357ac4b05b66e9ed373
Artur <artur.davtyan.96@inbox.ru>
2020-04-19 03:16:33 +0400

    revert

---
+++ homework/Narek_Iskandaryan/Javascript/task_3/js/main1.js
@@ -0,0 +1,25 @@
+// main1.js
+
+const q1 = '2 + 2'; 					const a1 = '4';
+const q2 = 'What is the name of the earth satellite'; 	const a2 = 'moon';
+const q3 = '2 + 10'; 					const a3 = '12';
+let score = 0;
+
+askQuestion(q1, a1);
+askQuestion(q2, a2);
+askQuestion(q3, a3);
+
+function askQuestion(q, a) {
+	const userAnswer = prompt(q);
+	const validAnswer = answerValidation(userAnswer);
+	if (validAnswer && validAnswer === a) score++;
+}
+
+function answerValidation(a) {
+	if (a) { 
+		return a.toLowerCase().trim();
+	} 
+	return false;
+}
+
+console.log(`Your score is: ${score}`);
