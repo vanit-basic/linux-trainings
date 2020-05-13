1bff0aa8f0c1ca0f757a1646205893e9982a7e2a
Srbuhi_P <panchoyansrbuhi@gmail.com>
2020-05-13 07:10:48 +0400

    add homework5

--- homework/srbuhi-panchoyan/js/homework4/index.js
+++ homework/srbuhi-panchoyan/js/homework4/index.js
@@ -1,4 +1,63 @@
-let carlist = [];
+let carlist = [
+	{
+		type: "Mercedes",
+		model: "e300",
+		color: "black",
+		date: "1990",
+		vin: "548762",
+		accident: "flood"
+	},
+	{
+                type: "BMW",
+                model: "X6",
+                color: "white",
+                date: "2010",
+		vin: "5478963",
+		accident: "accident"
+        },
+	{
+                type: "Audi",
+                model: "a6",
+                color: "greeen",
+                date: "2005",
+		vin: "214789",
+		accident: "other"
+        },
+	{
+                type: "Opel",
+                model: "Astra G",
+                color: "blue",
+                date: "1998",
+		vin: "987452",
+		accident: "flood"
+        },
+	{
+                type: "WolksVagen",
+                model: "Golf",
+                color: "red",
+                date: "1995",
+		vin: "6547125",
+		accident: "accident"
+        },
+	{
+                type: "Mercedes",
+                model: "c200",
+                color: "red",
+                date: "1999",
+		vin: "145789",
+		accident: "other"
+        },
+	{
+                type: "Mercedes",
+                model: "ML500",
+                color: "blue",
+                date: "2011",
+		vin: "967412",
+                accident: "flood"
+        },
+
+
+];
 let cars = {};
 
 function addCar() {
@@ -9,15 +68,17 @@ function addCar() {
 	let vin = document.getElementById("vin").value.trim();
 	let accident = document.getElementById("accident").value.trim();
 
+	//carlist.find(
+	//	{if(cars.vin === vin) alert('This car already exsist')}); 
 	cars = { 
-		"type": type,  
-		"model": model, 
-		"color": color,
-		"date": date,
-		"vin": vin,
-		"accident": accident
+		type: "type",  
+		model: "model", 
+		color: "color",
+		date: "date",
+		vin: "vin",
+		accident: "accident"
 	};
-
+	
 	carlist.push(cars);
 	for(let [key, value] of Object.entries(carlist[carlist.length- 1])){
 
@@ -27,12 +88,12 @@ function addCar() {
 }
 
 
-
 function findCarByVin (vin){if (cars.vin === vin){return car;}}
 
 function findCarByAccident (accident) {if (cars.accident === accident){return car;}}
 
-function findCarByType (type) {if (cars.type === type){return car;}}
-	
-
+function findCarByType (type) {
+	carlist.filter(if(cars.type === type)console.log(cars))
+}
+function findCarByColor(color) {if (cars.color === color){return car;}}
 
